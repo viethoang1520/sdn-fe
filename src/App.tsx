e80184d6559495065a705f27a40802bc4e9d2162
viethoang1520 <viethoang1520.dev@gmail.com>
2025-06-27 15:53:19 +0000

    add payment state pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import AuthPage from "./components/AuthPage";
 import TrainSchedule from "./components/TrainSchedule";
+import SuccessPayment from "./components/SuccessPayment";
+import ErrorPayment from "./components/ErrorPayment";
 import routes from "tempo-routes";
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<AuthPage />} />
           <Route path="/schedule" element={<TrainSchedule />} />
+          <Route path="/payment/success" element={<SuccessPayment />} />
+          <Route path="/payment/error" element={<ErrorPayment />} />
         </Routes>
         {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
       </>
